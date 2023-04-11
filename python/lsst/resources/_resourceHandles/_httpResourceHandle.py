e13cbb68753ffcbe7b24dc2f529043910402fa34
Tim Jenness <tjenness@lsst.org>
2023-04-11 13:20:48 -0700

    Check that we have got units of bytes before parsing Content-Range

--- python/lsst/resources/_resourceHandles/_httpResourceHandle.py
+++ python/lsst/resources/_resourceHandles/_httpResourceHandle.py
@@ -181,14 +181,17 @@ class HttpReadResourceHandle(BaseResourceHandle[bytes]):
         # server.
         if "Content-Range" in resp.headers:
             content_range = resp.headers["Content-Range"]
-            _, range_string = content_range.split(" ")
-            range, total = range_string.split("/")
-            if "-" in range:
-                _, end = range.split("-")
-                end_pos = int(end)
-                if total != "*":
-                    if end_pos == int(total) - 1:
-                        self._eof = True
+            units, range_string = content_range.split(" ")
+            if units == "bytes":
+                range, total = range_string.split("/")
+                if "-" in range:
+                    _, end = range.split("-")
+                    end_pos = int(end)
+                    if total != "*":
+                        if end_pos == int(total) - 1:
+                            self._eof = True
+            else:
+                self._log.warning("Requested byte range from server but instead got: %s", content_range)
 
         # Try to guess that we overran the end. This will not help if we
         # read exactly the number of bytes to get us to the end and so we
